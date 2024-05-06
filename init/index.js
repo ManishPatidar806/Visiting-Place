9a5dee989ab79bf6dcdbff75db3accc852ac15a9
ManishPatidar <manishpatidar1180@gmail.com>
2024-05-06 19:08:52 +0530

    Create Some changes in Database

--- init/index.js
+++ init/index.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const initData = require('./data.js');
 const Listing = require('../models/listing.js');
-const MONGO_URL = 'mongodb://127.0.0.1:27017/wonderlust'
+// const MONGO_URL = 'mongodb://127.0.0.1:27017/wonderlust'
+const dburl = process.env.ATLASDB_URL;
+
 
 main().then(()=>{
     console.log("Mongoose is Connected");
@@ -10,12 +12,12 @@ main().then(()=>{
 })
 
 async function main(){
-    await mongoose.connect(MONGO_URL);
+    await mongoose.connect("mongodb+srv://mp6012217:ManishPatidar@cluster0.2qdlyid.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 }
 
 const initDb = async ()=>{
    await Listing.deleteMany({});
-   initData.data = initData.data.map((obj)=>({...obj,owner:"6634d2357b69f4eca8297f4c"}));
+   initData.data = initData.data.map((obj)=>({...obj,owner:"6638d667596be02fe9eb3777"}));
     await Listing.insertMany(initData.data);
     console.log("Data was Initilise");
 }
